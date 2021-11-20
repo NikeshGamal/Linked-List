d4a04f121879d58c33ef3fdee7647064cf248374
Nikesh Gamal <nikesh.191624@ncit.edu.np>
2021-11-20 12:19:07 +0545

    Linked List Simple Program

---
+++ LinkedListInsetion.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+
+//For Creation of a node
+class Node{
+    private:
+       int data;
+       Node* next;
+    public:
+       void setData(int data){
+         this->data=data;
+       }
+
+       void setNext(Node* ptr){
+         this->next=ptr;    
+       }
+
+       void display(){
+           cout<<"Data: "<<data<<endl;
+       }
+};
+
+int main(){
+  //For node
+  //Null Pointer
+  Node* nullPointer= NULL;
+   //Starting node is called head
+
+   Node* head= new Node();
+   Node* first= new Node();
+   Node* second= new Node();
+
+   //User Inputs
+   int headInput,secondInput,firstInput;
+   cout<<"Enter data for head"<<endl;
+   cin>>headInput;
+
+   cout<<"Enter data for first"<<endl;
+   cin>>firstInput;
+
+   cout<<"Enter data for Second"<<endl;
+   cin>>secondInput;
+
+
+   //For Head
+    head->setData(headInput);
+    head->setNext(first);
+    cout<<"Data of Head"<<endl;
+    head->display();
+
+  //For first
+    first->setData(firstInput);
+    first->setNext(nullPointer);
+     cout<<"Data of First"<<endl;
+    first->display();
+
+    //For second
+    second->setData(secondInput);
+    second->setNext(nullPointer);
+     cout<<"Data of First"<<endl;
+    second->display();
+
+   //New Node
+   Node* newNode= new Node();
+   newNode->setNext(head);
+   head->setNext(newNode);
+
+   //For newNode
+   first->setData(firstInput);
+    first->setNext(nullPointer);
+    cout<<"Data of First"<<endl;
+    first->display();
+  return 0;
+}
